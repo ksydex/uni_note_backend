1bf2b310fb679da223945ac6256c4455fd277be3
Artyom <38377534+ksydex@users.noreply.github.com>
2022-06-21 05:35:50 +0300

    added endpoint remove

---
+++ src/UniNote.Api/Migrations/20220621023536_SomeUpd.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniNote.Api.Migrations
+{
+    public partial class SomeUpd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "created_by_user_id",
+                table: "tags",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "notes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_tags_created_by_user_id",
+                table: "tags",
+                column: "created_by_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_tags_users_created_by_user_id",
+                table: "tags",
+                column: "created_by_user_id",
+                principalTable: "users",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_tags_users_created_by_user_id",
+                table: "tags");
+
+            migrationBuilder.DropIndex(
+                name: "ix_tags_created_by_user_id",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "created_by_user_id",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "notes");
+        }
+    }
+}
